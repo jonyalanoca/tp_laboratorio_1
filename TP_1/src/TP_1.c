6aeffdca5a09d223f730f674b11929a772e664dd
jonyalanoca <jonyalanoca2@gmail.com>
2022-04-04 13:11:26 -0300

    creando repo

---
+++ TP_1/src/TP_1.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : TP_1.c
+ Author      : Alanoca Aduviri Jonathan Gerardo
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout,NULL);
+
+	return EXIT_SUCCESS;
+}
